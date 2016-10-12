42637cb633bfff2df6eed045030b5671249b7b9c
Ben McClure <ben.mcclure@gmail.com>
2016-10-12 15:22:02 -0500

    Add back missing source plugin

---
+++ src/Plugin/migrate/source/AluminumImportCsvMediaEntity.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BMcClure
+ * Date: 10/4/2016
+ * Time: 5:19 PM
+ */
+
+namespace Drupal\aluminum_import\Plugin\migrate\source;
+
+/**
+ * Class AluminumImportCsvMediaEntity
+ * @package Drupal\aluminum_import\Plugin\migrate\source
+ *
+ * @MigrateSource(
+ *   id = "aluminum_import_csv_media_entity"
+ * )
+ */
+class AluminumImportCsvMediaEntity extends AluminumImportCsv {
+
+}
